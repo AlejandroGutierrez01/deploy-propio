c9961571e07f855ce771ad5a2e6ab8e9fa538112
AlejandroGutierrez01 <alejandro.gutierrez01@epn.edu.ec>
2024-12-01 19:37:05 -0500

    Update index.js 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Punto de arranque de todo el web server
 import app from './server.js'
 
-
-app.listen(app.get('port'),()=>{console.log(`Server on port ${app.get('port')}`)})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
